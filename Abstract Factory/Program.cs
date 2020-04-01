f8e64e16d3d5d80ddf36f60cdc8d2f611368e5bf
itmaxxx <dmitrievmaks77@gmail.com>
2020-04-02 00:37:00 +0300

    + Abstract Factory/Program.cs + Factory Method/Program.cs

---
+++ Abstract Factory/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+	abstract class Continent
+	{
+		public string Name { get; protected set; }
+
+		public abstract Herbivore CreateHerbivore();
+		public abstract Carnivore CreateCarnivore();
+	}
+
+	class Africa : Continent
+	{
+		private Africa() { }
+		private static Africa africa;
+
+		public static Africa GetContinent()
+		{
+			if (africa == null)
+			{
+				africa = new Africa();
+			}
+
+			return africa;
+		}
+
+		public override Herbivore CreateHerbivore()
+		{
+			return new Wildebeest();
+		}
+
+		public override Carnivore CreateCarnivore()
+		{
+			return new Lion();
+		}
+	}
+
+	class NorthAmerica : Continent
+	{
+		private NorthAmerica() { }
+		private static NorthAmerica NA;
+
+		public static NorthAmerica GetContinent()
+		{
+			if (NA == null)
+			{
+				NA = new NorthAmerica();
+			}
+
+			return NA;
+		}
+
+		public override Herbivore CreateHerbivore()
+		{
+			return new Elk();
+		}
+
+		public override Carnivore CreateCarnivore()
+		{
+			return new Wolf();
+		}
+	}
+
+	class Eurasion : Continent
+	{
+		private Eurasion() { }
+		private static Eurasion eurasion;
+
+		public static Eurasion GetContinent()
+		{
+			if (eurasion == null)
+			{
+				eurasion = new Eurasion();
+			}
+
+			return eurasion;
+		}
+
+		public override Herbivore CreateHerbivore()
+		{
+			return new Wildebeest();
+		}
+
+		public override Carnivore CreateCarnivore()
+		{
+			return new Tiger();
+		}
+	}
+
+	abstract class Herbivore
+	{
+		public string Name { get; protected set; }
+		public int Weight { get; protected set; }
+		public bool Life { get; protected set; }
+
+		public virtual void EatGrass()
+		{
+			Weight += 10;
+
+			Console.WriteLine($"{Name} eats grass and gets 10 weight");
+		}
+	}
+
+	class Wildebeest : Herbivore
+	{
+		public Wildebeest()
+		{
+			Name = "Wildebeest";
+			Weight = 100;
+			Life = true;
+		}
+	}
+
+	class Bison : Herbivore
+	{
+		public Bison()
+		{
+			Name = "Bison";
+			Weight = 120;
+			Life = true;
+		}
+	}
+
+	class Elk : Herbivore
+	{
+		public Elk()
+		{
+			Name = "Elk";
+			Weight = 180;
+			Life = true;
+		}
+	}
+
+	abstract class Carnivore
+	{
+		public string Name { get; protected set; }
+		public int Power { get; protected set; }
+
+		public virtual void Eat(Herbivore herbivore)
+		{
+			if (this.Power > herbivore.Weight)
+			{
+				Power += 10;
+
+				Console.WriteLine($"{Name} eaten {herbivore.Name} and got 10 power");
+			}
+			else
+			{
+				Power -= 10;
+
+				Console.WriteLine($"{Name} tried to eat {herbivore.Name} but failed and lost 10 power");
+			}
+		}
+	}
+
+	class Lion : Carnivore
+	{
+		public Lion()
+		{
+			Name = "Lion";
+			Power = 100;
+		}
+	}
+
+	class Wolf : Carnivore
+	{
+		public Wolf()
+		{
+			Name = "Wolf";
+			Power = 80;
+		}
+	}
+
+	class Tiger : Carnivore
+	{
+		public Tiger()
+		{
+			Name = "Tiger";
+			Power = 70;
+		}
+	}
+
+	class AnimalWorld
+	{
+		public void MealsHerbivores(Continent continent)
+		{
+			var herbivore = continent.CreateHerbivore();
+
+			herbivore.EatGrass();
+		}
+
+		public void NutritionCarnivores(Continent continent)
+		{
+			var herbivore = continent.CreateHerbivore();
+			var carnivore = continent.CreateCarnivore();
+
+			carnivore.Eat(herbivore);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			AnimalWorld AW = new AnimalWorld();
+
+			Africa africa = Africa.GetContinent();
+
+			AW.MealsHerbivores(africa);
+			AW.NutritionCarnivores(africa);
+		}
+	}
+}
---
+++ Factory Method/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace TetrisFactory
+{
+	class Color
+	{
+		byte red;
+		byte green;
+		byte blue;
+
+		public Color()
+		{
+			this.red = 0;
+			this.green = 0;
+			this.blue = 0;
+		}
+
+		public Color(byte red, byte green, byte blue)
+		{
+			SetColor(red, green, blue);
+		}
+
+		public void SetColor(byte red, byte green, byte blue)
+		{
+			this.red = red;
+			this.green = green;
+			this.blue = blue;
+		}
+
+		public byte GetRed()
+		{
+			return red;
+		}
+
+		public byte GetGreen()
+		{
+			return green;
+		}
+
+		public byte GetBlue()
+		{
+			return blue;
+		}
+	}
+
+	abstract class Figure
+	{
+		protected bool[,] geometry;
+		protected Color color;
+		protected string name;
+
+		public virtual void ShowInfo()
+		{
+			Console.WriteLine($"Name : {name}\nColor : r{color.GetRed()} g{color.GetGreen()} b{color.GetBlue()}");
+
+			ShowFigure();
+		}
+
+		public void ShowFigure()
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					if (geometry[i,j])
+					{
+						Console.Write("*");
+					}
+					else
+					{
+						Console.Write(" ");
+					}
+				}
+
+				Console.WriteLine();
+			}
+		}
+
+		public static Figure CreateFigure(int type)
+		{
+			Figure figure = null;
+
+			if (type == 1)
+				figure = new Figure1();
+			else if (type == 2)
+				figure = new Figure2();
+			else if (type == 3)
+				figure = new Figure3();
+
+			return figure;
+		}
+	}
+
+	class Figure1 : Figure
+	{
+		public Figure1()
+		{
+			geometry = new bool[4, 4]
+			{ 
+				{ true, true, true, false }, 
+				{ true, false, false, false }, 
+				{ true, false, false, false }, 
+				{ false, false, false, false }
+			};
+			color = new Color((byte) 0, (byte) 0, (byte) 0);
+			name = "Figure 1";
+		}
+	}
+
+	class Figure2 : Figure
+	{
+		public Figure2()
+		{
+			geometry = new bool[4, 4]
+			{
+				{ false, true, true, true },
+				{ false, false, false, true },
+				{ false, false, false, false },
+				{ false, false, false, false }
+			};
+			color = new Color((byte) 255, (byte) 255, (byte) 255);
+			name = "Figure 2";
+		}
+	}
+
+	class Figure3 : Figure
+	{
+		public Figure3()
+		{
+			geometry = new bool[4, 4]
+			{
+				{ false, false, false, false },
+				{ false, false, false, false },
+				{ true, false, false, false },
+				{ true, true, true, false }
+			};
+			color = new Color((byte) 121, (byte) 121, (byte) 121);
+			name = "Figure 3";
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Figure> figures = new List<Figure>();
+
+			figures.Add(Figure.CreateFigure(1));
+			figures.Add(Figure.CreateFigure(2));
+			figures.Add(Figure.CreateFigure(3));
+
+			for (int i = 0; i < figures.Count; i++)
+			{
+				figures.ElementAt(i).ShowInfo();
+			}
+		}
+	}
+}
