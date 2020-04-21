b5ae1782518871bb87db4b2119672deedd1afcf0
itmaxxx <dmitrievmaks77@gmail.com>
2020-04-21 14:11:10 +0300

    + Adapter (txt)/Program.cs + Factory Method (txt)/Program.cs

---
+++ Adapter (txt)/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Adapter
+{
+    class ArrowClock
+    {
+        private int HoursArrow;
+        private int MinutesArrow;
+        private bool AnteMeridian;    // true = AM / false = PM
+        
+        public ArrowClock(int hours, int minutes, bool anteMeridian)
+        {
+            HoursArrow = hours;
+            MinutesArrow = minutes;
+            AnteMeridian = anteMeridian;
+
+            if (hours >= 12 || hours <= 0) 
+            {
+                HoursArrow = 0;
+                AnteMeridian = true;
+            }
+
+            if (minutes > 59  || minutes < 0)
+            {
+                MinutesArrow = 0;
+            }
+        }
+
+        public void ShowTime() 
+        {
+            string AMPM = AnteMeridian ? "AM" : "PM";
+
+            Console.WriteLine($"Arrow Clock Time : \n{HoursArrow} : {MinutesArrow} {AMPM}\n");
+        }
+
+        public int GetHourArrowValue() 
+        { 
+            return HoursArrow; 
+        }
+        
+        public int GetMinuteArrowValue() 
+        { 
+            return MinutesArrow; 
+        }
+
+        public bool GetMeridiem() 
+        { 
+            return AnteMeridian; 
+        }
+    }
+
+    abstract class DigitalClock 
+    {
+        public abstract void ShowTime();
+    }
+
+    class DigitalClockAdapter : DigitalClock
+    {
+        private ArrowClock arrowclock;
+
+        public DigitalClockAdapter(ArrowClock clock) {
+            arrowclock = clock;
+        }
+
+        public override void ShowTime() 
+        {
+            int hours = arrowclock.GetHourArrowValue();
+            int minutes = arrowclock.GetMinuteArrowValue();
+            bool anteMeridian = arrowclock.GetMeridiem();
+
+            if (!anteMeridian) 
+                hours = hours + 12;
+
+            string AMPM = anteMeridian ? "AM" : "PM";
+            
+            Console.WriteLine($"Digital Clock Time :\n{hours} : {minutes} {AMPM}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalClock clock = new DigitalClockAdapter(new ArrowClock(6, 45, false));
+            clock.ShowTime();
+        }
+    }
+}
---
+++ Factory Method (txt)/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SmartphoneFactory
+{
+	abstract class Smartphone
+	{
+		protected string Name;
+		protected int RearCameraMP;
+		protected int FrontCameraMP;
+        protected double DisplaySize;
+        protected int Storage;
+        protected int RAM;
+
+		public virtual void ShowInfo()
+		{
+			Console.WriteLine($"Name : {Name}\nRear Camera MP : {RearCameraMP}\nFront Camera MP : {FrontCameraMP}\nDisplay Size : {DisplaySize}\nStorage : {Storage}\nRAM : {RAM}");
+		}
+
+		public static Smartphone CreateSmartphone(string modelName)
+		{
+			Smartphone smartphone = null;
+
+			if (modelName == "Samsung Note 10")
+				smartphone = new SamsungNote10();
+			else if (modelName == "Iphone X")
+				smartphone = new IphoneX();
+			else if (modelName == "Huawei P20")
+				smartphone = new HuaweiP20();
+
+			return smartphone;
+		}
+	}
+
+	class SamsungNote10 : Smartphone
+	{
+		public SamsungNote10()
+		{
+			Name = "Samsung Note 10";
+            RearCameraMP = 18;
+            FrontCameraMP = 10;
+            DisplaySize = 7.5;
+            Storage = 512;
+            RAM = 6;
+		}
+	}
+
+	class IphoneX : Smartphone
+	{
+		public IphoneX()
+		{
+			Name = "Iphone X";
+            RearCameraMP = 8;
+            FrontCameraMP = 16;
+            DisplaySize = 6.5;
+            Storage = 128;
+            RAM = 4;
+		}
+	}
+
+    class HuaweiP20 : Smartphone
+	{
+		public HuaweiP20()
+		{
+			Name = "Huawei P20";
+            RearCameraMP = 12;
+            FrontCameraMP = 22;
+            DisplaySize = 7.5;
+            Storage = 512;
+            RAM = 8;
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Smartphone> smartphones = new List<Smartphone>();
+
+			smartphones.Add(Smartphone.CreateSmartphone("Huawei P20"));
+			smartphones.Add(Smartphone.CreateSmartphone("Samsung Note 10"));
+			smartphones.Add(Smartphone.CreateSmartphone("Iphone X"));
+
+			for (int i = 0; i < smartphones.Count; i++)
+			{
+				smartphones.ElementAt(i).ShowInfo();
+			}
+		}
+	}
+}
