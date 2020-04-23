51b2718f3d477b21611c9234520cf5b02c4ce7bd
itmaxxx <dmitrievmaks77@gmail.com>
2020-04-23 13:56:58 +0300

    + Strategy (txt)/Program.cs

---
+++ Strategy (txt)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Strategy
+{
+    interface IPrint 
+    {
+        void Print();
+    }
+
+    class BlackAndWhiteCartridge : IPrint
+    {
+        public void Print()
+        {
+            System.Console.WriteLine("Black And White Print");
+        }
+    }
+
+    class ColourfulCartridge : IPrint
+    {
+        public void Print() 
+        {
+            System.Console.WriteLine("Colourful Print");
+        }
+    }
+
+    class Printer 
+    {
+        private IPrint _Printer;
+
+        public Printer(IPrint Printer) {
+            _Printer = Printer;
+        }
+
+        public void Print() 
+        {
+            _Printer.Print();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer BwPrinter = new Printer(new BlackAndWhiteCartridge());
+            BwPrinter.Print();
+
+            Printer CPrinter = new Printer(new ColourfulCartridge());
+            CPrinter.Print();
+        }
+    }
+}
