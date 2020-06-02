8d6178561cf47fd8f5461c7bea7cc714c1957f82
Max <dmitrievmaks77@gmail.com>
2020-06-02 13:37:14 +0300

    + Bridge

--- Patterns/Bridge/Program.cs
+++ Patterns/Bridge/Program.cs
@@ -6,15 +6,65 @@ using System.Threading.Tasks;
 
 namespace Bridge
 {
-
-    
     class Program
     {
+        interface Component
+        {
+            void ShowInfo();
+        }
+
+        class PC
+        {
+            private Component component;
+
+            public PC(Component _component)
+            {
+                component = _component;
+            }
+
+            public void ShowComponentInfo()
+            {
+                component.ShowInfo();
+            }
+        }
+
+        class GraphicsCard : Component
+        {
+            public void ShowInfo()
+            {
+                Console.WriteLine("It is graphic card info");
+            }
+        }
+
+        class Processor : Component
+        {
+            public void ShowInfo()
+            {
+                Console.WriteLine("It is processor info");
+            }
+        }
+
+        class HDD : Component
+        {
+            public void ShowInfo()
+            {
+                Console.WriteLine("It is HDD info");
+            }
+        }
+
+        class RAM : Component
+        {
+            public void ShowInfo()
+            {
+                Console.WriteLine("It is RAM info");
+            }
+        }
+
         static void Main(string[] args)
         {
-            
-
-            Console.WriteLine("Hello World!");
+            GraphicsCard graphicsCard = new GraphicsCard();
+            PC pc = new PC(graphicsCard);
+            pc.ShowComponentInfo();
         }
     }
 }
