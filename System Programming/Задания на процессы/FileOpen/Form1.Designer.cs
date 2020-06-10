deedf1f475a18adb1a5a24c6064e4efb7e61c7ca
Max <dmitrievmaks77@gmail.com>
2020-06-10 14:25:13 +0300

    Process

---
+++ System Programming/Задания на процессы/FileOpen/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FileOpen
+{
+    partial class FileLauncher
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialogExe = new System.Windows.Forms.OpenFileDialog();
+            this.buttonExe = new System.Windows.Forms.Button();
+            this.openFileDialogTxt = new System.Windows.Forms.OpenFileDialog();
+            this.buttonTxt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialogExe
+            // 
+            this.openFileDialogExe.Filter = "EXE files|*.exe";
+            this.openFileDialogExe.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialogExe_FileOk);
+            // 
+            // buttonExe
+            // 
+            this.buttonExe.Location = new System.Drawing.Point(12, 12);
+            this.buttonExe.Name = "buttonExe";
+            this.buttonExe.Size = new System.Drawing.Size(158, 23);
+            this.buttonExe.TabIndex = 0;
+            this.buttonExe.Text = "Open and start exe";
+            this.buttonExe.UseVisualStyleBackColor = true;
+            this.buttonExe.Click += new System.EventHandler(this.buttonExe_Click);
+            // 
+            // openFileDialogTxt
+            // 
+            this.openFileDialogTxt.Filter = "TXT files|*.txt";
+            this.openFileDialogTxt.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialogTxt_FileOk);
+            // 
+            // buttonTxt
+            // 
+            this.buttonTxt.Location = new System.Drawing.Point(12, 41);
+            this.buttonTxt.Name = "buttonTxt";
+            this.buttonTxt.Size = new System.Drawing.Size(158, 23);
+            this.buttonTxt.TabIndex = 1;
+            this.buttonTxt.Text = "Open and edit txt";
+            this.buttonTxt.UseVisualStyleBackColor = true;
+            this.buttonTxt.Click += new System.EventHandler(this.buttonTxt_Click);
+            // 
+            // FileLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 77);
+            this.Controls.Add(this.buttonTxt);
+            this.Controls.Add(this.buttonExe);
+            this.Name = "FileLauncher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Launcher";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialogExe;
+        private System.Windows.Forms.Button buttonExe;
+        private System.Windows.Forms.OpenFileDialog openFileDialogTxt;
+        private System.Windows.Forms.Button buttonTxt;
+    }
+}
+
---
+++ System Programming/Задания на процессы/FileOpen/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileOpen
+{
+    public partial class FileLauncher : Form
+    {
+        public FileLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExe_Click(object sender, EventArgs e)
+        {
+            openFileDialogExe.ShowDialog();
+        }
+
+        private void openFileDialogExe_FileOk(object sender, CancelEventArgs e)
+        {
+            if (e.Cancel)
+            {
+                return;
+            }
+
+            Process.Start(openFileDialogExe.FileName);
+        }
+
+        private void buttonTxt_Click(object sender, EventArgs e)
+        {
+            openFileDialogTxt.ShowDialog();
+        }
+
+        private void openFileDialogTxt_FileOk(object sender, CancelEventArgs e)
+        {
+            if (e.Cancel)
+            {
+                return;
+            }
+
+            Process.Start("notepad.exe", openFileDialogTxt.FileName);
+        }
+    }
+}
