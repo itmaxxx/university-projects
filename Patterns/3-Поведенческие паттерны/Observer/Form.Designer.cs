433c5027ffbbac58d8e833b17c428f66124d9ae4
Max <dmitrievmaks77@gmail.com>
2020-06-15 00:33:51 +0300

    Observer

---
+++ Patterns/3-Поведенческие паттерны/Observer/Form.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Observer
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.buttonUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUp.ForeColor = System.Drawing.Color.Black;
+            this.buttonUp.Location = new System.Drawing.Point(470, 10);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(50, 50);
+            this.buttonUp.TabIndex = 0;
+            this.buttonUp.Text = "Up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.buttonRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRight.ForeColor = System.Drawing.Color.Black;
+            this.buttonRight.Location = new System.Drawing.Point(526, 65);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(50, 50);
+            this.buttonRight.TabIndex = 1;
+            this.buttonRight.Text = "Right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.buttonDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDown.ForeColor = System.Drawing.Color.Black;
+            this.buttonDown.Location = new System.Drawing.Point(470, 121);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(50, 50);
+            this.buttonDown.TabIndex = 2;
+            this.buttonDown.Text = "Down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.buttonLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLeft.ForeColor = System.Drawing.Color.Black;
+            this.buttonLeft.Location = new System.Drawing.Point(414, 65);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(50, 50);
+            this.buttonLeft.TabIndex = 3;
+            this.buttonLeft.Text = "Left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(586, 181);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonUp);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Observer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonLeft;
+    }
+}
+
---
+++ Patterns/3-Поведенческие паттерны/Observer/Form.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observer
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+		public interface IObserver
+		{
+			void Update(ISubject subject, int x, int y);
+		}
+
+		public interface ISubject
+		{
+			void Add(IObserver observer);
+
+			void Remove(IObserver observer);
+
+		}
+
+		public class MovableButton : Button, IObserver
+		{
+			public void Update(ISubject subject, int x, int y)
+			{
+				Location = new Point(Location.X + x, Location.Y + y);
+			}
+		}
+
+		private class ControllerButton : ISubject
+		{
+			private List<IObserver> observers = new List<IObserver>();
+
+			public Button CButton { get; set; }
+
+			public ControllerButton(Button button)
+			{
+				CButton = button;
+			}
+
+			public void Add(IObserver observer)
+			{
+				observers.Add(observer);
+			}
+
+			public void Remove(IObserver observer)
+			{
+				observers.Remove(observer);
+			}
+
+			public void MoveObj(int x, int y)
+			{
+				foreach (var item in observers)
+				{
+					item.Update(this, x, y);
+				}
+			}
+		}
+
+		private void UI()
+		{
+			ControllerButton cButtonUp = new ControllerButton(buttonUp);
+			ControllerButton cButtonDown = new ControllerButton(buttonDown);
+			ControllerButton cButtonRight = new ControllerButton(buttonRight);
+			ControllerButton cButtonLeft = new ControllerButton(buttonLeft);
+
+			int marginTop = 10;
+			int marginLeft = 20;
+
+			for (int i = 0; i < 5; i++)
+			{
+				MovableButton button = new MovableButton();
+				button.Left = marginLeft;
+				button.Top = marginTop;
+				button.Name = "buttonID" + i;
+				button.Click += MoveButton;
+				button.Text = "Click to select";
+				button.BackColor = Color.White;
+				button.Width = 100;
+				button.FlatStyle = FlatStyle.Flat;
+				button.FlatAppearance.BorderSize = 1;
+				button.FlatAppearance.BorderColor = Color.LightGray;
+				Controls.Add(button);
+				marginTop += button.Height + 10;
+			}
+
+			void MoveButton(object sender, EventArgs eventArgs)
+			{
+				var button = (MovableButton)sender;
+
+				if (button.BackColor != Color.Green)
+				{
+					button.BackColor = Color.Green;
+					button.ForeColor = Color.White;
+					button.Text = "Selected";
+					cButtonUp.Add(button);
+					cButtonDown.Add(button);
+				}
+				else
+				{
+					button.BackColor = Color.White;
+					button.ForeColor = Color.Black;
+					button.Text = "Click to select";
+					cButtonUp.Remove(button);
+					cButtonDown.Remove(button);
+				}
+			}
+
+			cButtonUp.CButton.Click += ButtonUpClick;
+			cButtonDown.CButton.Click += ButtonDownClick;
+			cButtonRight.CButton.Click += ButtonRightClick;
+			cButtonLeft.CButton.Click += ButtonLeftClick;
+
+			void ButtonUpClick(object sender, EventArgs eventArgs)
+			{
+				cButtonUp.MoveObj(0, -3);
+			}
+			void ButtonDownClick(object sender, EventArgs eventArgs)
+			{
+				cButtonDown.MoveObj(0, +3);
+			}
+			void ButtonRightClick(object sender, EventArgs eventArgs)
+			{
+				cButtonUp.MoveObj(+3, 0);
+			}
+			void ButtonLeftClick(object sender, EventArgs eventArgs)
+			{
+				cButtonDown.MoveObj(-3, 0);
+			}
+		}
+
+		public Form()
+        {
+            InitializeComponent();
+			UI();
+        }
+	}
+}
