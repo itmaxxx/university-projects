3881eff2c144c7cff5f0fe35e6484ffa113bb15c
itmaxxx <dmitrievmaks77@gmail.com>
2020-06-24 14:06:31 +0300

    семафоры

---
+++ System Programming/4-Задания на синхронизацию/Семафоры/SemaphoreForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Semaphore
+{
+    partial class SemaphoreForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxWorking = new System.Windows.Forms.ListBox();
+            this.listBoxWaiting = new System.Windows.Forms.ListBox();
+            this.listBoxCreated = new System.Windows.Forms.ListBox();
+            this.labelWorking = new System.Windows.Forms.Label();
+            this.labelWaiting = new System.Windows.Forms.Label();
+            this.labelCreated = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.buttonCreateThread = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxWorking
+            // 
+            this.listBoxWorking.FormattingEnabled = true;
+            this.listBoxWorking.Location = new System.Drawing.Point(12, 66);
+            this.listBoxWorking.Name = "listBoxWorking";
+            this.listBoxWorking.Size = new System.Drawing.Size(120, 95);
+            this.listBoxWorking.TabIndex = 0;
+            this.listBoxWorking.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxWorking_MouseDoubleClick);
+            // 
+            // listBoxWaiting
+            // 
+            this.listBoxWaiting.FormattingEnabled = true;
+            this.listBoxWaiting.Location = new System.Drawing.Point(222, 66);
+            this.listBoxWaiting.Name = "listBoxWaiting";
+            this.listBoxWaiting.Size = new System.Drawing.Size(120, 95);
+            this.listBoxWaiting.TabIndex = 0;
+            // 
+            // listBoxCreated
+            // 
+            this.listBoxCreated.FormattingEnabled = true;
+            this.listBoxCreated.Location = new System.Drawing.Point(440, 66);
+            this.listBoxCreated.Name = "listBoxCreated";
+            this.listBoxCreated.Size = new System.Drawing.Size(120, 95);
+            this.listBoxCreated.TabIndex = 0;
+            this.listBoxCreated.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxCreated_MouseDoubleClick);
+            // 
+            // labelWorking
+            // 
+            this.labelWorking.AutoSize = true;
+            this.labelWorking.Location = new System.Drawing.Point(13, 47);
+            this.labelWorking.Name = "labelWorking";
+            this.labelWorking.Size = new System.Drawing.Size(75, 13);
+            this.labelWorking.TabIndex = 1;
+            this.labelWorking.Text = "Работающие:";
+            // 
+            // labelWaiting
+            // 
+            this.labelWaiting.AutoSize = true;
+            this.labelWaiting.Location = new System.Drawing.Point(219, 47);
+            this.labelWaiting.Name = "labelWaiting";
+            this.labelWaiting.Size = new System.Drawing.Size(73, 13);
+            this.labelWaiting.TabIndex = 1;
+            this.labelWaiting.Text = "Ожидающие:";
+            // 
+            // labelCreated
+            // 
+            this.labelCreated.AutoSize = true;
+            this.labelCreated.Location = new System.Drawing.Point(437, 47);
+            this.labelCreated.Name = "labelCreated";
+            this.labelCreated.Size = new System.Drawing.Size(67, 13);
+            this.labelCreated.TabIndex = 1;
+            this.labelCreated.Text = "Созданные:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 201);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(12, 185);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(97, 13);
+            this.labelCount.TabIndex = 1;
+            this.labelCount.Text = "Количество мест:";
+            // 
+            // buttonCreateThread
+            // 
+            this.buttonCreateThread.Location = new System.Drawing.Point(222, 198);
+            this.buttonCreateThread.Name = "buttonCreateThread";
+            this.buttonCreateThread.Size = new System.Drawing.Size(120, 23);
+            this.buttonCreateThread.TabIndex = 3;
+            this.buttonCreateThread.Text = "Создать поток";
+            this.buttonCreateThread.UseVisualStyleBackColor = true;
+            this.buttonCreateThread.Click += new System.EventHandler(this.buttonCreateThread_Click);
+            // 
+            // SemaphoreForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 233);
+            this.Controls.Add(this.buttonCreateThread);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelCreated);
+            this.Controls.Add(this.labelWaiting);
+            this.Controls.Add(this.labelWorking);
+            this.Controls.Add(this.listBoxCreated);
+            this.Controls.Add(this.listBoxWaiting);
+            this.Controls.Add(this.listBoxWorking);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "SemaphoreForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Семафоры";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxWorking;
+        private System.Windows.Forms.ListBox listBoxWaiting;
+        private System.Windows.Forms.ListBox listBoxCreated;
+        private System.Windows.Forms.Label labelWorking;
+        private System.Windows.Forms.Label labelWaiting;
+        private System.Windows.Forms.Label labelCreated;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Button buttonCreateThread;
+    }
+}
+
---
+++ System Programming/4-Задания на синхронизацию/Семафоры/SemaphoreForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semaphore
+{
+    public partial class SemaphoreForm : Form
+    {
+        public static int THREAD_COUNT = 0;
+
+        public int MAX_INITIAL_COUNT = 2;
+        public int MAX_COUNT = 2;
+
+        private static object lockObj = new object();
+
+        private static System.Threading.Semaphore semaphore;
+
+        public static List<ThreadObject> threadsCreated = new List<ThreadObject>();
+        public static List<ThreadObject> threadsWaiting = new List<ThreadObject>();
+        public static List<ThreadObject> threadsWorking = new List<ThreadObject>();
+        public class ThreadObject 
+        {
+            public int Index { get; set; } = 0;
+            public int Count { get; set; } = 0;
+            public Thread thread { get; set; }
+
+            public ThreadObject() 
+            {
+            }
+        }
+        public SemaphoreForm()
+        {
+            InitializeComponent();
+            semaphore = new System.Threading.Semaphore(MAX_INITIAL_COUNT, MAX_COUNT);
+        }
+
+        private void buttonCreateThread_Click(object sender, EventArgs e)
+        {
+            ThreadObject newThreadObject = new ThreadObject();
+            newThreadObject.thread = new Thread(new ThreadStart(DoWork));
+            newThreadObject.thread.Name = $"Поток №{++THREAD_COUNT}";
+            
+            threadsCreated.Add(newThreadObject);
+            listBoxCreated.Items.Add(newThreadObject.thread.Name);
+        }
+
+        private void UpdateListBoxes()
+        {
+            listBoxCreated.Items.Clear();
+            listBoxWaiting.Items.Clear();
+
+            foreach (ThreadObject t in threadsCreated)
+                listBoxCreated.Items.Add(t.thread.Name);
+            foreach (ThreadObject t in threadsWaiting)
+                listBoxWaiting.Items.Add(t.thread.Name);
+        }
+        public void DoWork() 
+        {
+            semaphore.WaitOne();
+
+            threadsWaiting.RemoveAt(0);
+
+            listBoxWaiting.BeginInvoke((MethodInvoker)(() =>
+            listBoxWaiting.Items.RemoveAt(0)
+            ));
+            
+
+            ThreadObject newObject = new ThreadObject
+            {
+                thread = Thread.CurrentThread,
+                Index = listBoxWorking.Items.Count
+            };
+
+            threadsWorking.Add(newObject);
+
+
+            listBoxWorking.BeginInvoke((MethodInvoker)(() =>
+            listBoxWorking.Items.Add(newObject.thread.Name)
+            ));
+
+            while (Thread.CurrentThread.IsAlive) 
+            {
+                lock (lockObj)
+                {
+                    listBoxWorking.BeginInvoke((MethodInvoker)(() =>
+                    listBoxWorking.Items[newObject.Index] = $"{newObject.thread.Name} - {++newObject.Count}"
+                    ));
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        private void UpdateWorkingThreadsIndexes() 
+        {
+            lock (lockObj) 
+            {
+               for (int i = 0; i < listBoxWorking.Items.Count; i++) 
+                {
+                    threadsWorking[i].Index = i;
+                }
+            }
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            // Поменять максимальное количество потоков в семафоре
+            // Проверить есть ли желающие в семафор
+        }
+        private void listBoxCreated_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Выбираю из созданных, какой поток должен стартовать как только в семафоре появится место
+            if (listBoxCreated.SelectedIndex != -1) 
+            {
+                threadsWaiting.Add(threadsCreated[listBoxCreated.SelectedIndex]);
+                threadsCreated.RemoveAt(listBoxCreated.SelectedIndex);
+
+                UpdateListBoxes();
+
+                threadsWaiting[threadsWaiting.Count - 1].thread.Start();
+            }
+        }
+        private void listBoxWorking_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBoxWorking.SelectedIndex != -1)
+            {
+                threadsWorking[listBoxWorking.SelectedIndex].thread.Abort();
+
+                threadsWorking.RemoveAt(listBoxWorking.SelectedIndex);
+                listBoxWorking.Items.RemoveAt(listBoxWorking.SelectedIndex);
+
+                UpdateWorkingThreadsIndexes();
+                semaphore.Release();
+            }
+        }
+    }
+}
