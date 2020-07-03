063cd2eb9ae2fc337872ff4b3e94d2955c9d9762
itmaxxx <dmitrievmaks77@gmail.com>
2020-07-03 03:32:58 +0300

    Time

---
+++ Network Programming/Time/Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UdpChat
+{
+    class Program
+    {
+        private static int localPort = 5000;
+        private static IPAddress remoteIPAddr = IPAddress.Parse("127.0.0.1");
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            try
+            {
+                ReceiveThreadProc();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        static void ReceiveThreadProc()
+        {
+            try
+            {
+                while (true)
+                {
+                    var udpClient = new UdpClient(localPort);
+
+                    IPEndPoint ipEnd = null;
+                    byte[] responce = udpClient.Receive(ref ipEnd);
+
+                    string strResult = Encoding.Unicode.GetString(responce);
+
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(strResult);
+
+                    udpClient.Close();
+                }
+            }
+            catch (SocketException sockEx)
+            {
+                Console.WriteLine($"Ошибка сокета: {sockEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Network Programming/Time/Server/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+	class Program
+	{
+        private static IPAddress remoteIPAddr = IPAddress.Parse("127.0.0.1");
+        private static int remotePort = 5000;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                while (true)
+				{
+                    SendData(DateTime.Now.ToString());
+
+                    Console.Clear();
+					Console.WriteLine(DateTime.Now.ToString());
+
+                    Thread.Sleep(1000);
+				}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        static void SendData(string datagramm)
+        {
+            var udpClient = new UdpClient();
+            var ipEnd = new IPEndPoint(remoteIPAddr, remotePort);
+
+            try
+            {
+                byte[] bytes = Encoding.Unicode.GetBytes(datagramm);
+
+                udpClient.Send(bytes, bytes.Length, ipEnd);
+            }
+            catch (SocketException sockEx)
+            {
+                Console.WriteLine($"Ошибка сокета: {sockEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+        }
+    }
+}
