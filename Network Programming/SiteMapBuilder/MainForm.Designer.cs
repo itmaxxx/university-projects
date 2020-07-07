e3830332f5384776e882de137dd34ec75df3c4cc
itmaxxx <dmitrievmaks77@gmail.com>
2020-07-07 03:38:28 +0300

    site map builder

---
+++ Network Programming/SiteMapBuilder/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SiteMap
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxMap = new System.Windows.Forms.TextBox();
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.textBoxUrl = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBoxMap
+			// 
+			this.textBoxMap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBoxMap.Location = new System.Drawing.Point(12, 70);
+			this.textBoxMap.Multiline = true;
+			this.textBoxMap.Name = "textBoxMap";
+			this.textBoxMap.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBoxMap.Size = new System.Drawing.Size(1575, 794);
+			this.textBoxMap.TabIndex = 0;
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Location = new System.Drawing.Point(1363, 12);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(224, 52);
+			this.buttonStart.TabIndex = 1;
+			this.buttonStart.Text = "Start";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+			// 
+			// textBoxUrl
+			// 
+			this.textBoxUrl.Location = new System.Drawing.Point(12, 19);
+			this.textBoxUrl.Name = "textBoxUrl";
+			this.textBoxUrl.Size = new System.Drawing.Size(1345, 31);
+			this.textBoxUrl.TabIndex = 2;
+			this.textBoxUrl.Text = "http://barabash.com";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1599, 876);
+			this.Controls.Add(this.textBoxUrl);
+			this.Controls.Add(this.buttonStart);
+			this.Controls.Add(this.textBoxMap);
+			this.Name = "MainForm";
+			this.Text = "Site Resources Downloader";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxMap;
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.TextBox textBoxUrl;
+	}
+}
+
---
+++ Network Programming/SiteMapBuilder/MainForm.cs
@@ -0,0 +1,115 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiteMap
+{
+	public partial class MainForm : Form
+	{
+		CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private string FixLink(string parsingUrl, string link)
+		{
+			Regex regex = new Regex($"{parsingUrl}");
+
+			if (!regex.Match(link).Success)
+			{
+				link = $"{parsingUrl}{((link.ElementAt(0) != '/' && parsingUrl.ElementAt(parsingUrl.Length - 1) != '/') ? "/" : "")}{link}";
+			}
+
+			return link;
+		}
+
+		private void ParseTag(HtmlAgilityPack.HtmlDocument doc, List<string> links, string tag, string param)
+		{
+			try
+			{
+				var tags = doc.DocumentNode.SelectNodes($"//{tag}");
+
+				if (tags != null)
+				{
+					foreach (HtmlNode t in tags)
+					{
+						var value = t.GetAttributeValue(param, "");
+
+						if (value.Length == 0 || value.IndexOf('#') != -1)
+						{
+							return;
+						}
+
+						if (links.Where(l => l.Equals(value)).ToArray().Length == 0)
+						{
+							links.Add(value);
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private List<string> ParsePageLinks(string url)
+		{
+			var links = new List<string>();
+
+			var doc = new HtmlWeb().Load(url);
+
+			ParseTag(doc, links, "a", "href");
+
+			return links;
+		}
+
+		private void ParseWebPage(string siteUrl, string url = "", string level = "")
+		{
+			if (url.Length == 0)
+				url = siteUrl;
+
+			Regex regex = new Regex($"{url}");
+
+			var links = ParsePageLinks(url);
+
+			textBoxMap.Text += $"{level}+{url}\r\n";
+
+			for (int i = 0; i < links.Count; i++)
+			{
+				links[i] = FixLink(siteUrl, links[i]);
+
+				textBoxMap.Text += $"{level}- {links[i]}\r\n";
+
+				if (!regex.Match(links[i]).Success)
+				{
+					continue;
+				}
+			}
+		}
+
+		private void buttonStart_Click(object sender, EventArgs e)
+		{
+			textBoxMap.Clear();
+
+			Task parse = new Task(() => {
+				buttonStart.Enabled = false;
+				ParseWebPage(textBoxUrl.Text);
+				buttonStart.Enabled = true;
+			});
+			parse.Start();
+		}
+	}
+}
