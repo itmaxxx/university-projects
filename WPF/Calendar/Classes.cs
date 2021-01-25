f31cbf8ca7fb1dab0c5e09342a2c807eaffd9378
itmaxxx <dmitrievmaks77@gmail.com>
2021-01-25 22:52:00 +0200

    Calendar

---
+++ WPF/Calendar/Classes.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    public enum DaysOfTheWeek
+    {
+        Monday, 
+        Tuesday, 
+        Wednesday, 
+        Thursday, 
+        Friday, 
+        Saturday, 
+        Sunday
+    }
+
+    public enum MonthsNames
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        Septemeber,
+        October,
+        November,
+        December
+    };
+
+    public class Day
+    {
+        public int Number { get; set; }
+        public DaysOfTheWeek DayOfTheWeek { get; set; }
+
+        public static int GetMonthDaysCount(MonthsNames month)
+        {
+            switch (month)
+            {
+                case MonthsNames.January:
+                    return 31;
+                case MonthsNames.February:
+                    return 28;
+                case MonthsNames.March:
+                    return 31;
+                case MonthsNames.April:
+                    return 30;
+                case MonthsNames.May:
+                    return 31;
+                case MonthsNames.June:
+                    return 30;
+                case MonthsNames.July:
+                    return 31;
+                case MonthsNames.August:
+                    return 31;
+                case MonthsNames.Septemeber:
+                    return 30;
+                case MonthsNames.October:
+                    return 31;
+                case MonthsNames.November:
+                    return 30;
+                case MonthsNames.December:
+                    return 31;
+                default: throw new Exception("Month doesn't exist");
+            }
+        }
+    }
+
+    public class Week
+    {
+        public string Monday { get; set; } = string.Empty;
+        public string Tuesday { get; set; } = string.Empty;
+        public string Wednesday { get; set; } = string.Empty;
+        public string Thursday { get; set; } = string.Empty;
+        public string Friday { get; set; } = string.Empty;
+        public string Saturday { get; set; } = string.Empty;
+        public string Sunday { get; set; } = string.Empty;
+    }
+
+    public class Month
+    {
+        public List<Day> Days { get; set; }
+        public MonthsNames Name { get; set; }
+    }
+
+    public class Year
+    {
+        public List<Month> Months { get; private set; }
+
+        public Year()
+        {
+            CreateCalendar();
+        }
+
+        private void CreateCalendar()
+        {
+            Months = new List<Month>(12);
+
+            int currentDayOfTheWeek = 0;
+
+            for (int currMonth = 0; currMonth < 12; currMonth++)
+            {
+				var month = new Month
+				{
+					Name = (MonthsNames)currMonth
+                };
+
+				int DaysInMonth = Day.GetMonthDaysCount((MonthsNames)currMonth);
+
+                month.Days = new List<Day>(DaysInMonth);
+
+                for (int currDay = 1; currDay <= month.Days.Capacity; currDay++)
+                {
+					var day = new Day
+					{
+						Number = currDay,
+						DayOfTheWeek = (DaysOfTheWeek)currentDayOfTheWeek
+                    };
+
+                    currentDayOfTheWeek++;
+
+                    // New week
+                    if (currentDayOfTheWeek > 6)
+					{
+                        currentDayOfTheWeek = 0;
+                    }
+
+                    month.Days.Add(day);
+                }
+
+                Months.Add(month);
+            }
+
+            Months.AddRange(Months);
+        }
+    }
+}
---
+++ WPF/Calendar/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+        public Year CurrentYear { get; set; }
+        public ObservableCollection<Week> CurrentMonth { get; set; }
+        public string CurrentMonthName { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CurrentYear = new Year();
+
+            CurrentMonthName = "December";
+
+            UpdateMonth(CurrentYear.Months[11]);
+        }
+
+        private void RadioButtonMonth_Checked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = (RadioButton)sender;
+
+            int selectedMonthTag = (int)(MonthsNames)Convert.ToInt32(radioButton.Tag);
+
+            Month selectedMonth = CurrentYear.Months[selectedMonthTag - 1];
+
+            CurrentMonthName = selectedMonth.Name.ToString();
+
+            UpdateMonth(selectedMonth);
+        }
+
+        private void UpdateMonth(Month month)
+        {
+            CurrentMonth = new ObservableCollection<Week>();
+
+            var currentWeek = new Week();
+
+            for (int i = 0; i < month.Days.Count; i++)
+            {
+                string dayStr = month.Days[i].Number.ToString();
+
+                switch (month.Days[i].DayOfTheWeek)
+                {
+                    case DaysOfTheWeek.Monday:
+                        currentWeek.Monday = dayStr;
+                        break;
+                    case DaysOfTheWeek.Tuesday:
+                        currentWeek.Tuesday = dayStr;
+                        break;
+                    case DaysOfTheWeek.Wednesday:
+                        currentWeek.Wednesday = dayStr;
+                        break;
+                    case DaysOfTheWeek.Thursday:
+                        currentWeek.Thursday = dayStr;
+                        break;
+                    case DaysOfTheWeek.Friday:
+                        currentWeek.Friday = dayStr;
+                        break;
+                    case DaysOfTheWeek.Saturday:
+                        currentWeek.Saturday = dayStr;
+                        break;
+                    case DaysOfTheWeek.Sunday:
+                        currentWeek.Sunday = dayStr;
+                        break;
+                }
+
+                if ((i == month.Days.Count - 1 && month.Days[i].DayOfTheWeek != DaysOfTheWeek.Sunday) 
+                    || month.Days[i].DayOfTheWeek == DaysOfTheWeek.Sunday)
+                {
+                    CurrentMonth.Add(currentWeek);
+
+                    currentWeek = new Week();
+                }
+            }
+
+            listViewCurrentMonth.ItemsSource = CurrentMonth;
+            textBlockCurrentMonthName.Text = CurrentMonthName;
+        }
+
+        private void ButtonSelectDay_Click(object sender, RoutedEventArgs e)
+		{
+            var button = (Button)sender;
+
+            if (button.Background == Brushes.Transparent)
+			{
+                button.Background = Brushes.Red;
+                button.Foreground = Brushes.White;
+            }
+            else if (button.Background == Brushes.Red)
+			{
+                button.Background = Brushes.Green;
+                button.Foreground = Brushes.White;
+            }
+            else if (button.Background == Brushes.Green)
+            {
+                button.Background = Brushes.Blue;
+                button.Foreground = Brushes.White;
+            }
+            else if (button.Background == Brushes.Blue && button.FontWeight == FontWeights.Bold)
+            {
+                button.Background = Brushes.Transparent;
+                button.Foreground = Brushes.Red;
+            }
+            else
+            {
+                button.Background = Brushes.Transparent;
+                button.Foreground = Brushes.Black;
+            }
+
+            if (button.Content.ToString().Length == 0)
+            {
+                return; 
+            }                
+
+            //MessageBox.Show($"{button.Content.ToString()} of {CurrentMonthName}");
+		}
+	}
+}
