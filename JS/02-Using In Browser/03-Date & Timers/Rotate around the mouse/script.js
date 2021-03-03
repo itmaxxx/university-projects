9520446ec93db4bea720542b7a80f254c71693bf
itmaxxx <dmitrievmaks77@gmail.com>
2021-03-03 12:53:28 +0200

    rotatable object around mouse

---
+++ JS/02-Using In Browser/03-Date & Timers/Rotate around the mouse/script.js
@@ -0,0 +1,16 @@
+let rotatableObject;
+
+function handleMouseMove(e) {
+  console.log(e);
+
+  rotatableObject.style.top = e.clientY - 20 + 'px';
+  rotatableObject.style.left = e.clientX - 20 + 'px';
+}
+
+function hangHandlers() {
+  rotatableObject = document.getElementById('element');
+
+  document.getElementsByClassName('app')[0].addEventListener('mousemove', handleMouseMove);
+}
+
+hangHandlers();
