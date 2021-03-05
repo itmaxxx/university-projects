fd7601aea1435a410c32bc8e17abd11d37ca4514
itmaxxx <dmitrievmaks77@gmail.com>
2021-03-05 14:11:21 +0200

    fix for timer

--- JS/02-Using In Browser/03-Date & Timers/02-setInterval/script.js
+++ JS/02-Using In Browser/03-Date & Timers/02-setInterval/script.js
@@ -4,37 +4,43 @@ function loadDate() {
     minutes = date.getMinutes().toString(),
     seconds = date.getSeconds().toString();
 
-  if (hours[0]) {
+  if (hours[0] && hours[1]) {
     hoursFirst.setAttribute('src', `/img/${hours[0]}.gif`);
   } else {
-    hoursFirst.setAttribute('src', `/img/blank.gif`);
+    hoursFirst.setAttribute('src', `/img/0.gif`);
   }
   if (hours[1]) {
     hoursSecond.setAttribute('src', `/img/${hours[1]}.gif`);
+  } else if (hours[0]) {
+    hoursSecond.setAttribute('src', `/img/${hours[0]}.gif`);
   } else {
-    hoursSecond.setAttribute('src', `/img/blank.gif`);
+    hoursSecond.setAttribute('src', `/img/0.gif`);
   }
 
-  if (minutes[0]) {
+  if (minutes[0] && minutes[1]) {
     minutesFirst.setAttribute('src', `/img/${minutes[0]}.gif`);
   } else {
-    minutesFirst.setAttribute('src', `/img/blank.gif`);
+    minutesFirst.setAttribute('src', `/img/0.gif`);
   }
   if (minutes[1]) {
     minutesSecond.setAttribute('src', `/img/${minutes[1]}.gif`);
+  } else if (minutes[0]) {
+    minutesSecond.setAttribute('src', `/img/${minutes[0]}.gif`);
   } else {
-    minutesSecond.setAttribute('src', `/img/blank.gif`);
+    minutesSecond.setAttribute('src', `/img/0.gif`);
   }
 
-  if (seconds[0]) {
+  if (seconds[0] && seconds[1]) {
     secondsFirst.setAttribute('src', `/img/${seconds[0]}.gif`);
   } else {
-    secondsFirst.setAttribute('src', `/img/blank.gif`);
+    secondsFirst.setAttribute('src', `/img/0.gif`);
   }
   if (seconds[1]) {
     secondsSecond.setAttribute('src', `/img/${seconds[1]}.gif`);
+  } else if (seconds[0]) {
+    secondsSecond.setAttribute('src', `/img/${seconds[0]}.gif`);
   } else {
-    secondsSecond.setAttribute('src', `/img/blank.gif`);
+    secondsSecond.setAttribute('src', `/img/0.gif`);
   }
 }
 
