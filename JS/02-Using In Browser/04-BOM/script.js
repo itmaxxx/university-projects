8108a85fdba23e342659027dfee1b5c43d627e3d
itmaxxx <dmitrievmaks77@gmail.com>
2021-03-11 09:53:03 +0200

    gallery

---
+++ JS/02-Using In Browser/04-BOM/script.js
@@ -0,0 +1,24 @@
+function zoom(imgNum) {
+  let win = window.open('', `Viewing image P${imgNum}.jpg`);
+  win.document.write(`<html><head><title>Viewing image P${imgNum}.jpg</title><style>* { padding: 0; margin: 0; }</style></head><body style="overflow: hidden;"><div style="width: 100vw; height: 100vh; background-image: url('/img/P${imgNum}.JPG'); background-size: cover; background-position: center;"></div></body></html>`);
+}
+
+function handleMouseOver(imgNum) {
+  document.title = `Zoom image P${imgNum}.jpg`;
+}
+
+function handleMouseOut() {
+  document.title = 'Gallery';
+}
+
+function hangHandlers() {
+  let images = document.getElementsByClassName('gallery__image');
+
+  Array.from(images).forEach((img, index) => {
+    img.addEventListener('click', () => zoom(index));
+    img.addEventListener('mouseover', () => handleMouseOver(index));
+    img.addEventListener('mouseout', () => handleMouseOut());
+  })
+}
+
+hangHandlers();
