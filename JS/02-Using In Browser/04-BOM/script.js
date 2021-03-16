5466c10c6ece54ca513222188e8b3182ed34716d
itmaxxx <dmitrievmaks77@gmail.com>
2021-03-16 13:17:04 +0200

    gallery sizes

--- JS/02-Using In Browser/04-BOM/script.js
+++ JS/02-Using In Browser/04-BOM/script.js
@@ -1,10 +1,15 @@
 function zoom(imgNum) {
-  let win = window.open('', `Viewing image P${imgNum}.jpg`);
+  let win = window.open('', `P${imgNum}.jpg`);
   win.document.write(`<html><head><title>Viewing image P${imgNum}.jpg</title><style>* { padding: 0; margin: 0; }</style></head><body style="overflow: hidden;"><div style="width: 100vw; height: 100vh; background-image: url('/img/P${imgNum}.JPG'); background-size: cover; background-position: center;"></div></body></html>`);
 }
 
 function handleMouseOver(imgNum) {
-  document.title = `Zoom image P${imgNum}.jpg`;
+  image_preview.src = `/img/P${imgNum}.jpg`;
+  
+  let width = image_preview.width, height = image_preview.height;
+  
+
+  document.title = `${width}x${height}px P${imgNum}.jpg`;
 }
 
 function handleMouseOut() {
