d9df367ff3ddf24eaa66e7fd9334208200112e83
itmaxxx <dmitrievmaks77@gmail.com>
2021-03-26 13:52:15 +0200

    clock ready

--- JS/02-Using In Browser/08-Canvas/script.js
+++ JS/02-Using In Browser/08-Canvas/script.js
@@ -9,21 +9,20 @@ function clockPainting() {
 
   let ctx = document.getElementById("clock").getContext("2d");
 
-  ctx.clearRect(0, 0, 150, 150);
+  ctx.clearRect(0, 0, 300, 300);
 
-  ctx.drawImage(img, 0, 0, 150, 150);
+  ctx.drawImage(img, 0, 0, 300, 300);
   
   ctx.restore(); // достаем последний сохраненный контекст из стэка
   ctx.save();
 
-  ctx.translate(75, 75);
+  ctx.translate(150, 150);
   ctx.scale(0.4, 0.4);
   ctx.rotate(-Math.PI / 2);
 
   ctx.strokeStyle = "black";
   ctx.fillStyle = "black";
   ctx.lineWidth = 8;
-  ctx.lineCap = "round";
 
   ctx.save();
   ctx.restore(); // достаем последний сохраненный контекст из стэка
@@ -33,13 +32,13 @@ function clockPainting() {
   ctx.rotate((Math.PI / 6) * hr +
       (Math.PI / 360) * min +
       (Math.PI / 21600) * sec);
-  ctx.lineWidth = 14;
+  ctx.lineWidth = 10;
 
   ctx.beginPath();
   ctx.moveTo(-20, 0);
 
   // линия почти до часовых меток
-  ctx.lineTo(80, 0);
+  ctx.lineTo(150, 0);
   ctx.stroke();
   ctx.restore();
 
@@ -48,11 +47,11 @@ function clockPainting() {
   // минутная стрелка
   ctx.rotate((Math.PI / 30 * min) +
       (Math.PI / 1800) * sec);
-  ctx.lineWidth = 10;
+  ctx.lineWidth = 6;
 
   ctx.beginPath();
   ctx.moveTo(-28, 0);
-  ctx.lineTo(112, 0);
+  ctx.lineTo(200, 0);
   ctx.stroke();
   ctx.restore();
 
@@ -62,11 +61,11 @@ function clockPainting() {
   ctx.rotate(sec * Math.PI / 30);
   ctx.strokeStyle = "#D40000"; // цвет контура
   ctx.fillStyle = "#D40000";
-  ctx.lineWidth = 6;
+  ctx.lineWidth = 2;
 
   ctx.beginPath();
   ctx.moveTo(-30, 0);
-  ctx.lineTo(83, 0);
+  ctx.lineTo(240, 0);
   ctx.stroke();
   ctx.restore();
 
@@ -75,7 +74,6 @@ function clockPainting() {
 
 img.addEventListener("load", function() {
   window.onload = function() {
-    setInterval(clockPainting, 1000); // функция, перерисовывающая часы
-    //через равные промежутки времени
+    setInterval(clockPainting, 1000);
   }
 }, false);
