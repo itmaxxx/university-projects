037301582274db0fddb27222a3115d428ec7d372
itmaxxx <dmitrievmaks77@gmail.com>
2021-04-15 10:40:15 +0300

    jquery 2

--- JS/03-JQuery/02/script.js
+++ JS/03-JQuery/02/script.js
@@ -5,7 +5,24 @@ $(function() {
 
     $('#resizable2').height($('body').height() - $('#resizable1').height());
   });
-  $("#resizable2").resizable({ minWidth: 300, maxWidth: 300 });
+  $('#toggler1').on('click', () => { 
+    $('.side-menu__resizable').toggle('slide', 500); 
 
-  $('.side-menu').toggle('slide', 500);
+    $('.toggle-icon-right').toggleClass('toggle-icon-left');
+  });
+
+  $("#resizable3").resizable({ minWidth: 300, maxWidth: 300 });
+  $('#resizable3').on('resize', (e) => { 
+    console.log($('#resizable3').height(), $('#resizable4').height(), $('body').height());
+
+    if ($('#resizable4').height() <= 200 && ($('body').height() - $('#resizable3').height()) < 200) {
+      $('#resizable3').height($('body').height() - 200)
+
+      return $('#resizable4').height(200);
+    }
+
+    $('#resizable4').height($('body').height() - $('#resizable3').height());
+  });
+
+  $('.ui-resizable-s').html('<img src="equals.png"/>');
 });
