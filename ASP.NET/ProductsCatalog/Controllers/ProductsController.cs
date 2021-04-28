6864a9de29a49209491079b33c1acc8810eda285
itmaxxx <dmitrievmaks77@gmail.com>
2021-04-28 12:24:45 +0300

    products catalog

---
+++ ASP.NET/ProductsCatalog/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductsCatalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsCatalog.Controllers
+{
+    public class ProductsController : Controller
+    {
+        readonly Products products = new Products();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            Product product = products.ProductsList.Find(p => p.Id == id);
+
+            return View(product);
+        }
+    }
+}
---
+++ ASP.NET/ProductsCatalog/Models/ProductModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsCatalog.Models
+{
+    public class Product
+    {
+        public int Id { get; private set; }
+        public string Image { get; private set; }
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public Product(int id, string image, string name, double price)
+        {
+            Id = id;
+            Image = image;
+            Name = name;
+            Price = price;
+        }
+    }
+
+    public class Products
+    {
+        public List<Product> ProductsList;
+
+        public Products()
+        {
+            ProductsList = new List<Product>()
+            {
+                new Product(1, "iphone11_128gb_black.jpeg", "Iphone 11 128gb Black", 920),
+                new Product(2, "iphone11_128gb_white.jpeg", "Iphone 11 128gb White", 940),
+                new Product(3, "iphone11_128gb_green.jpeg", "Iphone 11 128gb Green", 900),
+                new Product(4, "iphone11_128gb_purple.jpeg", "Iphone 11 128gb Purple", 900),
+                new Product(5, "iphone11_128gb_black.jpeg", "Iphone 11 128gb Black", 920),
+                new Product(6, "iphone11_128gb_white.jpeg", "Iphone 11 128gb White", 940),
+                new Product(7, "iphone11_128gb_green.jpeg", "Iphone 11 128gb Green", 900),
+                new Product(8, "iphone11_128gb_purple.jpeg", "Iphone 11 128gb Purple", 900)
+            };
+        }
+    }
+}
